cf987e077d28989d525e676da8a9c9912deebe80
J. R. Schmid <jrs+git@weitnahbei.de>
2020-03-05 10:00:49 +0100

    M0ar ... sensible for now.

--- plotdata.py
+++ plotdata.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sqlite3
+from os import path
 from datetime import datetime, date, time, timedelta
 
 # TODO: why not working?
@@ -37,24 +38,30 @@ for offset in range(n_days):
     first_min = datetime.combine(first_day + timedelta(days = offset), time(0, 0, 1))
     last_min = datetime.combine(first_day + timedelta(days = offset), time(23, 59, 59))
     date_str = first_min.strftime('%Y-%m-%d')
+    date_today_str = date.today().strftime('%Y-%m-%d')
+    filename = "day_%s.png" % date_str
 
-    sql_params = [first_min, last_min]
-    data = pd.read_sql_query("SELECT time, kelvin from %s WHERE time BETWEEN ? AND ?" % TCS, sql_connection, params = sql_params)
+    if not path.isfile(filename) or date_str == date_today_str:
+        sql_params = [first_min, last_min]
+        data = pd.read_sql_query("SELECT time, kelvin from %s WHERE time BETWEEN ? AND ?" % TCS, sql_connection, params = sql_params)
 
-    data = data.set_index('time').groupby(pd.Grouper(freq='T')).mean().reset_index()
+        # TODO: works fine but there are holes in the data - why?!
+        # data = data.set_index('time').groupby(pd.Grouper(freq='T')).mean().reset_index()
 
-    if DEBUG:
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-            print(data)
+        if DEBUG:
+            with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+                print(data)
+        else:
+          print("Plotting %s" % date_str)
 
-    fig, ax = plt.subplots(figsize=(12,6))
-    plot_title = "Light temperature (K) throughout %s" % date_str
-    data.plot(kind='line', title=plot_title, x='time', y='kelvin', legend=False, ax=ax)
-    ax.set_xlabel('Hour')
-    ax.set_xlim(datetime.combine(first_day + timedelta(days = offset), time(3, 0, 0)), datetime.combine(first_day + timedelta(days = offset), time(23, 0, 0)))
-    ax.set_ylabel('Temperature (K)')
-    ax.set_ylim(1500, 10000)
+        fig, ax = plt.subplots(figsize=(12,6))
+        plot_title = "Light temperature (K) throughout %s" % date_str
+        data.plot(kind='line', title=plot_title, x='time', y='kelvin', legend=False, ax=ax)
+        ax.set_xlabel('Hour')
+        ax.set_xlim(datetime.combine(first_day + timedelta(days = offset), time(3, 0, 0)), datetime.combine(first_day + timedelta(days = offset), time(23, 0, 0)))
+        ax.set_ylabel('Temperature (K)')
+        ax.set_ylim(1500, 10000)
 
-    plt.savefig("day_%s.png" % date_str)
+        plt.savefig(filename)
 
 sql_connection.close()
